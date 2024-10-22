19d3931e3f08c3863dfeb0dadf1f52b0d20a0aa8
JazValerio <valeriogonzalesjahaziel@gmail.com>
2024-10-21 19:47:14 -0600

    Ajustes finales primera entrega del juego

--- js/components/enemy.js
+++ js/components/enemy.js
@@ -33,7 +33,6 @@ export class Enemy {
       //add sprite to group
       this.enemies.add(enemy);
 
-      enemy.originalX = item.x;
       enemy.speed = 100;
 
     });
@@ -41,17 +40,18 @@ export class Enemy {
   }
 
   update() {
-    //chatgpt: Ayudame a controlar que el enemigo se mueva a la vez que el player, cuando este se acerca a un rango específico este lo siga, toma en base este codigo (base del método)
+    //chatgpt: Ayudame a controlar que el enemigo se mueva a la vez que el player, cuando este se acerca a un rango específico este lo siga, toma en base este codigo (base del método pegado a chat)
     this.enemies.getChildren().forEach((enemy) => {
       let distance = Phaser.Math.Distance.Between(
         this.player.get().x, this.player.get().y,
         enemy.x, enemy.y
       );
 
-      if (distance < 75) { 
+      if (distance < 120) { 
+        enemy.setVelocity(100);
         this.scene.physics.moveToObject(enemy, this.player.get(), enemy.speed);
       } else {
-        this.scene.physics.moveTo(enemy, enemy.originalX, enemy.y, enemy.speed);
+        enemy.setVelocity(0);
       }
     });
   }
